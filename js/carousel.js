85e756d49c33f2e723fcf059e7c37b6a555c805e
Th0mzzz <contatothomazvmendes@gmail.com>
2023-07-25 10:16:59 -0300

    att

--- js/carousel.js
+++ js/carousel.js
@@ -1,8 +1,13 @@
-const slides = document.querySelector(".slides");
-const slideWidth = document.querySelector(".slide").clientWidth;
+function carrosel (carouselId){
+const container = document.getElementById(carouselId)
+const slides = container.querySelector(".slides");
+const slideWidth = container.querySelector(".slide").clientWidth;
+const prev = container.querySelector('.prevBtn')
+const next = container.querySelector('.nextBtn')
 let currentSlide = 0;
 let touchStartX = 0;
 let touchEndX = 0;
+
 
 function prevSlide() {
     currentSlide--;
@@ -48,68 +53,15 @@ function handleSwipe() {
 slides.addEventListener("touchstart", handleTouchStart);
 slides.addEventListener("touchend", handleTouchEnd);
 
+prev.addEventListener("click", prevSlide);
+next.addEventListener('click',nextSlide)
+
+}
+
+
+carrosel("carouselInicial")
+carrosel("recomendados")
 
 
 // Carrosel Itens
 
-function carroselItens(carroselId){
-
-
-    const carouselContainer = document.querySelector(carroselId);
-    const itens = carouselContainer.querySelector(".itens");
-    const itemWidth = carouselContainer.querySelector(".item").clientWidth;
-    let currentSlide = 0;
-    let touchStartX = 0;
-    let touchEndX = 0;
-
-  
-    function updateCarousel() {
-      const offsetX = -currentSlide * itemWidth;
-      itens.style.transform = `translateX(${offsetX}px)`;
-    }
-  
-    function prevSlide() {
-      currentSlide--;
-      if (currentSlide < 0) {
-        currentSlide = itens.children.length - 1;
-      }
-      updateCarousel();
-    }
-  
-    function nextSlide() {
-      currentSlide++;
-      if (currentSlide >= itens.children.length) {
-        currentSlide = 0;
-      }
-      updateCarousel();
-    }
-    function handleTouchStart(event) {
-        touchStartX = event.touches[0].clientX;
-    }
-    
-    function handleTouchEnd(event) {
-        touchEndX = event.changedTouches[0].clientX;
-        handleSwipe();
-    }
-    
-    function handleSwipe() {
-        const swipeThreshold = 50; // Adjust this value to control sensitivity
-        const deltaX = touchEndX - touchStartX;
-    
-        if (deltaX > swipeThreshold) {
-            prevSlide();
-        } else if (deltaX < -swipeThreshold) {
-            nextSlide();
-        }
-    }
-    
-    itens.addEventListener("touchstart", handleTouchStart);
-    itens.addEventListener("touchend", handleTouchEnd);
-    
-  
-    carouselContainer.querySelector(".prev-button").addEventListener("click", prevSlide);
-    carouselContainer.querySelector(".next-button").addEventListener("click", nextSlide);
-
-}
-
-carroselItens('#recomendados')
