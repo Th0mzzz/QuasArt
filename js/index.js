553cfcdcd6fa15e091fb794989f654611e5a9d16
Th0mzzz <contatothomazvmendes@gmail.com>
2023-08-28 11:57:55 -0300

    att

--- js/index.js
+++ js/index.js
@@ -5,7 +5,9 @@ const searchModal =  document.querySelector('#searchModal')
 
 const list = document.querySelectorAll('.list');
 const nav = document.querySelector('.navigation');
+
 list.forEach(item => item.addEventListener('click', function(e){
+
 	list.forEach(li => {
         li.classList.remove('active')
         searchModal.classList.remove('search_ativo')
---
+++ js/plus.js
@@ -0,0 +1,21 @@
+const modal = document.querySelector('.pubModal');
+const planetBtn = document.querySelectorAll(".planet_btn");
+const closeModal = document.querySelector('.close_btn');
+
+planetBtn.forEach( btn =>{
+
+btn.addEventListener("click", ()=>{
+    modal.style.display = 'flex'
+
+    if(btn.classList.contains(''))
+
+})})
+
+
+closeModal.addEventListener("click", ()=>{
+    modal.style.display = 'none'
+})
+
+
+
+
