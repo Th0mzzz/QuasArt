acfec174e5023d389e474f07a22eb29313e4b7ce
Th0mzzz <contatothomazvmendes@gmail.com>
2023-09-11 12:00:33 -0300

    att

--- js/tags.js
+++ js/tags.js
@@ -2,23 +2,27 @@ const tagsTextArea = document.getElementById('tags');
 const tagsPreview = document.getElementById('tags-preview');
 
 tagsTextArea.addEventListener('keydown', function(e) {
-  if (e.key === 'Enter') {
-      e.preventDefault();
-  }
+ 
 });
 
+tagsTextArea.addEventListener('keydown', function(e) {
 
-
-tagsTextArea.addEventListener('input', function() {
-  const tags = tagsTextArea.value.split(/[ ,]+/);
-  tagsPreview.innerHTML = '';
-
+  if (e.key === 'Enter' || e.key === 'Space' ) {
+    e.preventDefault();
+    const tags = tagsTextArea.value.split(/[ ,]+/);
+    tagsPreview.innerHTML = '';
+  
   tags.forEach(function(tag) {
     if (tag.trim() !== '') {
       const tagElement = document.createElement('span');
       tagElement.classList.add('tag');
-      tagElement.innerText = `#${tag.trim()}`;
+      const maxTagLength = 15; 
+      const trimmedTag = tag.trim().slice(0, maxTagLength);
+      tagElement.innerText = `#${trimmedTag}`;
+      
       tagsPreview.appendChild(tagElement);
+      
     }
   });
+}
 });
