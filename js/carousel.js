f7104686aa7fc29b1f68db9a03fd20eeb1e6c6b0
Thomzzz <contatothomazvmendes@gmail.com>
2023-10-09 14:25:31 -0300

    att

--- js/carousel.js
+++ js/carousel.js
@@ -1,47 +1,37 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-function initCustomCarousel(carouselSelector, options) {
+  function initCustomCarousel(carouselSelector, options) {
     $(carouselSelector).owlCarousel(options);
   }
-  
+
 
   const inicialOptions = {
     center: true,
     loop: true,
     margin: 0,
-    nav:false,
-    responsiveClass: true,
-    autoplayTimeout:5000,
-    responsive: {
-      0: {
-        items: 1,
-      },
-      768: {
-        items: 1,
-      },
-      1024: {
-        items: 1,
-      },
-    },
-  };
-  
+    nav: false,
+    autoplayTimeout: 5000,
+    items: 1,
+
+  }
+
 
   const itensOptions = {
-    stagePadding:50,
-    margin:5,
-    nav:false,
-    loop:true,
+    stagePadding: 50,
+    margin: 5,
+    nav: false,
+    loop: true,
     responsiveClass: true,
-    
+
     responsive: {
       0: {
-        
+
         items: 2,
-        stagePadding:10,
+        stagePadding: 10,
       },
-      420:{
-        items:2,
-        stagePadding:15,
+      420: {
+        items: 2,
+        stagePadding: 15,
       },
       768: {
         items: 4,
@@ -49,21 +39,21 @@ function initCustomCarousel(carouselSelector, options) {
       1024: {
         items: 6,
       },
-     
+
     },
   };
 
   const resenhasOptions = {
-    stagePadding:100,
-    center:true,
+    stagePadding: 100,
+    center: true,
     loop: true,
-    nav:false,
+    nav: false,
     margin: 20,
     responsiveClass: true,
     responsive: {
       0: {
         items: 1,
-        stagePadding:10,
+        stagePadding: 10,
       },
       768: {
         items: 1,
@@ -74,42 +64,41 @@ function initCustomCarousel(carouselSelector, options) {
     },
   };
   const tagsOptions = {
-    center:false,
+    center: false,
     loop: true,
-    nav:false,
+    nav: false,
     margin: 5,
-    stagePadding:10,
+    stagePadding: 10,
     responsive: {
       0: {
         items: 1,
-        autoWidth:true,
+        autoWidth: true,
       },
       420: {
         items: 3,
       },
       768: {
         items: 4,
-        autoWidth:false,
+        autoWidth: false,
       },
       1024: {
         items: 5,
-        margin:10,
-        autoWidth:false,
+        margin: 10,
+        autoWidth: false,
       },
       1440: {
         items: 7,
-        margin:10,
-        autoWidth:false,
+        margin: 10,
+        autoWidth: false,
       }
     },
   };
-  
+
   initCustomCarousel(".inicial", inicialOptions);
   initCustomCarousel(".itens", itensOptions);
   initCustomCarousel(".resenhas", resenhasOptions);
   // initCustomCarousel(".videos", tagsOptions);
   initCustomCarousel(".tags-carousel", tagsOptions);
-  
 
-  });
-  
+
+});
