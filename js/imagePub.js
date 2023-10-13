ed824ae1b9889cc97b49f435e211d6a28b88dcb1
Th0mzzz <contatothomazvmendes@gmail.com>
2023-10-13 13:15:47 -0300

    att

--- js/imagePub.js
+++ js/imagePub.js
@@ -20,14 +20,19 @@ document.getElementById('inputImage_resenha').addEventListener('change', functio
         
     });
 
-
-    document.getElementById('inputVideo').addEventListener('change', function(event) {
+    const inputVideo = document.getElementById('inputVideo')
+    const feedVideo = document.querySelector(".feedback-video")
+    inputVideo.addEventListener('change', function(event) {
 
         const file = event.target.files[0];
         const extension = file.name.split('.').pop().toLowerCase();
     
         if (extension === 'mp4') {
-    
+
+            inputVideo.classList.remove("is-invalid")
+            feedVideo.classList.add("d-none")
+            feedVideo.classList.remove("d-block")
+
             const reader = new FileReader();
     
             reader.onload = function() {
@@ -38,7 +43,12 @@ document.getElementById('inputImage_resenha').addEventListener('change', functio
         reader.readAsDataURL(file);
         
         } else {
-            alert('O tipo de arquivo não é suportado.');
+            if(inputVideo.value.trim() === 0){
+                inputVideo.value = ""
+            }
+            inputVideo.classList.add('is-invalid')
+            feedVideo.classList.remove("d-none")
+            feedVideo.classList.add("d-block")
         }
             
         });
