433d2b6cb64be0cfba3fb7c069451053d6e90ae1
Th0mzzz <contatothomazvmendes@gmail.com>
2023-10-31 12:03:28 -0300

    att

--- js/validation.js
+++ js/validation.js
@@ -1,67 +1,43 @@
 document.addEventListener('DOMContentLoaded', function () {
     const form = document.querySelector(".formConta")
     const inputsConta = document.querySelectorAll(".input-conta")
-    
-
 
     form.addEventListener('submit', function (event) {
+        event.preventDefault();
 
         inputsConta.forEach(input => {
-
+            let isValid = true
             let paiInput = input.parentNode
-
             let feedback = paiInput.querySelector(".feedback")
 
             if (input.value.trim() === '') {
-
                 input.classList.add('is-invalid');
-                event.preventDefault();
-                
-            } else {
-                input.classList.remove('is-invalid');
-                event.preventDefault();
-                window.location.href = "perfil.html"
-            }
-
-            if(input.classList.contains("is-invalid")){
+                isValid = false
+            } 
+            if (input.classList.contains("senha") && input.value.length < 8) {
+                input.classList.add("is-invalid")
+                feedback.innerHTML = "No mínimo 8 caracteres!"
                 feedback.style.display = "block"
-            }else {
-                feedback.style.display = "none"
-                
+                isValid = false
             }
 
-            if(input.classList.contains("senha")){
-                
-                 if(input.value.length < 8){
-                    event.preventDefault();
-                    input.classList.add("is-invalid")
-                    feedback.innerHTML = "No minimo 8 caracteres!"
-                    feedback.style.display = "block"
-                    
-                 }
-            }
+            input.addEventListener('input', function () {
+                if (input.value.trim() !== '') {
+                    input.classList.remove('is-invalid');
+                    feedback.style.display = "none"
+                }
+            });
 
-        
-
-
-
-
-
-        input.addEventListener('input', function () {
-            if (input.value.trim() !== '') {
-                input.classList.remove('is-invalid');
+            if (input.classList.contains("is-invalid")) {
+                feedback.style.display = "block"
+            } else {
                 feedback.style.display = "none"
             }
-            
+        })
 
-        });
-
-
-
-
-       
-
+   
+        if (document.querySelectorAll('.is-invalid').length === 0) {
+            window.location.href = "perfil.html";
+        }
     })
-
-
-})})
+})
