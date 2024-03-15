3a60a12663913c1fe21795daef9be02117b64c9d
Th0mzzz <contatothomazvmendes@gmail.com>
2024-03-15 15:24:53 -0300

    atualização de js carousel e termino do footer e da landing page

--- assets/js/carouselMembro.js
+++ assets/js/carouselMembro.js
@@ -1,44 +1,27 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const cards = document.querySelectorAll('.card__membro');
-    let currentIndex = 0;
 
-    function updateCards() {
-        const width = window.innerWidth;
-        cards.forEach((card, index) => {
-            card.style.display = 'none'; // Esconde todos os cards inicialmente
-        });
 
-        if (width > 768) { // Para desktops
-            // Mostra o card atual
-            cards[currentIndex].style.display = 'block';
-            // Tenta mostrar os próximos dois cards, se existirem
-            if (cards[currentIndex + 1]) {
-                cards[currentIndex + 1].style.display = 'block';
-            }
-            if (cards[currentIndex + 2]) {
-                cards[currentIndex + 2].style.display = 'block';
-            }
-        } else { // Para celulares
-            // Mostra apenas o card atual
-            cards[currentIndex].style.display = 'block';
+const cards = document.querySelectorAll(".card__membro")
+let indexAtual = 0;
+
+function atualizarCard() {
+    cards.forEach((card, indexCard) => {
+        card.style.transform = `translateX(-${indexAtual * 100}%)`;
+
+        if (indexCard === indexAtual) {
+            card.classList.add("active")
+        } else {
+            card.classList.remove("active")
         }
-    }
-
-    document.getElementById('next').addEventListener('click', function () {
-        if (currentIndex < cards.length - 1) {
-            currentIndex += 1;
-            updateCards();
-        }
-    });
-
-    document.getElementById('previous').addEventListener('click', function () {
-        if (currentIndex > 0) {
-            currentIndex -= 1;
-            updateCards();
-        }
-    });
-
-    // Atualiza os cards inicialmente e também ao redimensionar a janela
-    updateCards();
-    window.addEventListener('resize', updateCards);
+    })
+}
+document.getElementById('previous').addEventListener('click', () => {
+    indexAtual = (indexAtual - 1 + cards.length) % cards.length;
+    atualizarCard();
 });
+document.getElementById('next').addEventListener('click', () => {
+    indexAtual = (indexAtual + 1) % cards.length;
+    atualizarCard();
+});
+
+
+atualizarCard()
