981c43d731833e8c1fc8d5da33cafe42ec167a3a
th0mzzz <contatothomazvmendes@gmail.com>
2024-03-30 23:00:23 -0300

    atualizaçao do projeto para modelo nodeJS, ejs e express

---
+++ app/public/js/landingNav.js
@@ -0,0 +1,30 @@
+const nav = document.querySelector(".nav");
+const menuBtn = document.querySelector(".landing__nav-btn")
+const menuContainer = document.querySelector(".menu-container")
+const menuItens = document.querySelectorAll(".nav__item")
+// Menu mobile
+
+menuBtn.addEventListener("click", () => {
+    menuContainer.classList.toggle("show")
+    menuContainer.classList.contains("show") ? menuBtn.querySelector("i").className = "bi bi-x-lg " : menuBtn.querySelector("i").className = "bi bi-list ";
+
+})
+
+menuItens.forEach(item => item.addEventListener("click", () => { menuContainer.classList.remove("show") }))
+
+// animação nav
+let currentScrollY = window.scrollY
+
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY > currentScrollY) {
+        nav.style.transform = "translateY(-100%)"
+        currentScrollY = window.scrollY
+        menuContainer.classList.contains("show") ? menuContainer.classList.remove("show") : ""
+    }
+    if (window.scrollY < currentScrollY) {
+        nav.style.transform = "translateY(0%)"
+        currentScrollY = window.scrollY
+
+    }
+})
