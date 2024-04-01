a62e5197f800efe39422fccb50e407790f1982be
th0mzzz <contatothomazvmendes@gmail.com>
2024-04-01 16:35:52 -0300

    att

--- app/routes/router.js
+++ app/routes/router.js
@@ -5,4 +5,11 @@ router.get("/", function (req, res) {
     res.render("pages/index");
 });
 
+router.get("/entrar", function (req, res){
+    res.render("pages/entrar");
+});
+
+router.get("/cadastrar", function (req, res){
+    res.render("pages/cadastrar");
+});
 module.exports = router;
