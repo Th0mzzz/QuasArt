55aaaa5f641f507876bd11f0e01a7d72297a5123
th0mzzz <contatothomazvmendes@gmail.com>
2024-04-09 08:20:21 -0300

    atualização de css e inicio da página de login

--- app/routes/router.js
+++ app/routes/router.js
@@ -5,11 +5,11 @@ router.get("/", function (req, res) {
     res.render("pages/index");
 });
 
-router.get("/entrar", function (req, res){
-    res.render("pages/entrar");
+router.get("/entrar", function (req, res) {
+    res.render("pages/template-login", { page: "../partial/template-login/login" });
 });
 
-router.get("/cadastrar", function (req, res){
-    res.render("pages/cadastrar");
+router.get("/cadastrar", function (req, res) {
+    res.render("pages/template-login", { page: "../partial/template-login/cadastro" });
 });
 module.exports = router;
