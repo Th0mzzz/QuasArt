6b1e490e9abee549def0ff1025266a005aa5c01f
th0mzzz <contatothomazvmendes@gmail.com>
2024-04-23 16:52:15 -0300

    atualização do erros e termino do aside

--- app/public/js/colorMode.js
+++ app/public/js/colorMode.js
@@ -1,16 +1,22 @@
 const html = document.querySelector("html")
 const modo = localStorage.getItem('modo');
+const btnMode = document.getElementById('btnMode');
+const iconMode = btnMode.querySelector("i")
 if (modo === 'dark') {
     html.classList.add('dark');
+    iconMode.className = "bi bi-brightness-high"
 }
 
-const btnMode = document.getElementById('btnMode');
 btnMode.addEventListener('click', () => {
     if (html.classList.contains('dark')) {
         html.classList.remove('dark');
+        iconMode.className = "bi bi-moon"
+
         localStorage.setItem('modo', 'light');
     } else {
         html.classList.add('dark');
+        iconMode.className = "bi bi-brightness-high"
+
         localStorage.setItem('modo', 'dark');
     }
 });
