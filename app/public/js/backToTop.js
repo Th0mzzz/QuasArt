90c8e0b31f8e7d0ee8e90c990548d878f0d994a7
th0mzzz <contatothomazvmendes@gmail.com>
2024-04-24 17:32:10 -0300

    att

--- app/public/js/backToTop.js
+++ app/public/js/backToTop.js
@@ -1,7 +1,7 @@
 const backBtn = document.querySelector(".back-btn")
 
 window.addEventListener("scroll", () => {
-    if (window.scrollY >= 1000) {
+    if (window.scrollY >= 800) {
         backBtn.classList.add("ativo")
     }else{
         backBtn.classList.remove("ativo")
