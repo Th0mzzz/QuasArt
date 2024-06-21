37a79917f4100d3c896fb7b5d85aaa00b286c790
Th0mzzz <contatothomazvmendes@gmail.com>
2024-06-21 09:48:16 -0300

    att cpf-validação

--- app/controller/usuariosController.js
+++ app/controller/usuariosController.js
@@ -32,6 +32,43 @@ const usuariosController = {
                     throw new Error("CPF já em uso! Tente outro");
                 }
                 return true;
+            })
+            .bail()
+            .custom(async cpf => {
+                cpf = cpf.replace(/[^\d]+/g, '');
+                if (cpf.length !== 11 || /^(\d)\1+$/.test(cpf)) {
+                    throw new Error('CPF inválido');
+                }
+                let soma = 0;
+                let resto;
+                for (let i = 1; i <= 9; i++) {
+                    soma += parseInt(cpf.substring(i - 1, i)) * (11 - i);
+                }
+                resto = (soma * 10) % 11;
+
+                if (resto === 10 || resto === 11) {
+                    resto = 0;
+                }
+                if (resto !== parseInt(cpf.substring(9, 10))) {
+                    throw new Error('CPF inválido');
+                }
+
+                soma = 0;
+
+                // Validação do segundo dígito verificador
+                for (let i = 1; i <= 10; i++) {
+                    soma += parseInt(cpf.substring(i - 1, i)) * (12 - i);
+                }
+                resto = (soma * 10) % 11;
+
+                if (resto === 10 || resto === 11) {
+                    resto = 0;
+                }
+                if (resto !== parseInt(cpf.substring(10, 11))) {
+                    throw new Error('CPF inválido');
+                }
+
+                return true;
             }),
         //  verifica se o telefone é valido e faz o mesmo caso do cpf, so que para numero
         body("contato")
