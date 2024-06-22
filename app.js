641bc20b8292df048e484e2d7fae2125b936e5f8
th0mzzz <contatothomazvmendes@gmail.com>
2024-06-22 19:07:13 -0300

    att

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const session = require("express-session")
 const app = express();
-const port = 3000;
+const port = 3306;
 app.use((session({
   secret: "QuasartSession",
   resave: false,
