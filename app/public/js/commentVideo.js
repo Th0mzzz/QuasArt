4670b6318e5d582bbd5f548707635106a7f4064c
th0mzzz <contatothomazvmendes@gmail.com>
2024-06-26 15:21:03 -0300

    add denuncias modais, correção e alteração de front end

--- app/public/js/commentVideo.js
+++ app/public/js/commentVideo.js
@@ -1,10 +1,13 @@
-const btnOpenComments = document.querySelector("#btnOpenComments")
+const btnOpenComments = document.querySelectorAll(".btnOpenComments")
 const btnCloseComments = document.querySelector("#btnCloseComments")
 const videoCommentsContainer = document.querySelector("#videoComments")
 
-btnOpenComments.addEventListener("click", () => {
-    videoCommentsContainer.classList.toggle("aberto")
-});
+btnOpenComments.forEach(btn => {
+    console.log(btn)
+    btn.addEventListener("click", () => {
+        videoCommentsContainer.classList.toggle("aberto")
+    });
+})
 btnCloseComments.addEventListener("click", () => {
     videoCommentsContainer.classList.remove("aberto")
 });
