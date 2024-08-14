b5f1e63b0edaba6c9d837929638967e69d6c31fd
Th0mzzz <contatothomazvmendes@gmail.com>
2024-08-14 11:46:45 -0300

    att

--- app/public/js/abrirModal.js
+++ app/public/js/abrirModal.js
@@ -3,19 +3,20 @@ const btnsOpenModal = document.querySelectorAll("[data-openmodal]")
 
 
 btnsOpenModal.forEach(btn => {
-console.log(btn)
-    btn.addEventListener("click", () => {
-      let modalSelecionado = document.querySelector(btn.dataset.openmodal);
-      console.log(modalSelecionado)
-      modalSelecionado.classList.add("show");
-    });
+  console.log(btn)
+  btn.addEventListener("click", () => {
+    let modalSelecionado = document.querySelector(btn.dataset.openmodal);
+    console.log(modalSelecionado)
+    modalSelecionado.classList.add("show");
+  });
 
+});
+
+modals.forEach(modal => {
+  const closeBtn = modal.querySelector(".closeBtn");
+  closeBtn?.addEventListener("click", () => {
+    modal.classList.remove("show");
   });
-  
-  modals.forEach(modal => {
-    const closeBtn = modal.querySelector(".closeBtn");
-    closeBtn?.addEventListener("click", () => {
-      modal.classList.remove("show");
-    });
-  });
-  
+});
+
+
