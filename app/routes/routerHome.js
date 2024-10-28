6daa35bcd8179414540ecee1c898b7f29da3969d
Th0mzzz <contatothomazvmendes@gmail.com>
2024-10-28 14:52:37 -0300

    att

--- app/routes/routerHome.js
+++ app/routes/routerHome.js
@@ -912,7 +912,7 @@ router.post("/criarComandante",
             const assinatura = await preApproval.create({
                 body: {
                     preapproval_plan_id: planoId,
-                    payer_email:'TESTUSER908042050',
+                    payer_email:'testemail@example.com',
                     back_url: `${process.env.URL_BASE}/feedback-assinatura`,
                     reason: 'ComandantePlus',
                     status: 'pending',
